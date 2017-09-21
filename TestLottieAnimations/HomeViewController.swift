09ef50ea30ac844851185eda0ca7dbefc09b9840
William Wilson <will@letote.com>
2017-09-21 11:02:21 -0700

    Removed main storyboard

---
+++ TestLottieAnimations/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TestLottieAnimations
+//
+//  Created by William Wilson on 9/21/17.
+//  Copyright © 2017 LeTote. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
