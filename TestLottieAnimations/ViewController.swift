bd921e4d09a808c7059fe3a31b1fc969ba925a2e
William Wilson <will@letote.com>
2017-09-21 10:51:56 -0700

    Initial commit

---
+++ TestLottieAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestLottieAnimations
+//
+//  Created by William Wilson on 9/21/17.
+//  Copyright © 2017 LeTote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
